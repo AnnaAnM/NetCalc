b2bb62e461f06be0124a5e17ccd287c519652ce8
Anna <annaandreevna.m@gmail.com>
2016-12-20 20:30:45 +0300

    -[fix]  Небольшие исправления

--- app/src/main/java/asolovyev/ru/netcalc/ClassfulFragment.java
+++ app/src/main/java/asolovyev/ru/netcalc/ClassfulFragment.java
@@ -23,7 +23,6 @@ public class ClassfulFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.classful, null);
-        return view;
+        return inflater.inflate(R.layout.classful, container, false);
     }
 }
