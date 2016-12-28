0bda0ec57bc4ef8303b23fe135d6d7032ccae316
Anna <annaandreevna.m@gmail.com>
2016-12-28 20:48:30 +0300

    -[fix]  Небольшие исправления

--- app/src/main/java/asolovyev/ru/netcalc/CalculatorApp.java
+++ app/src/main/java/asolovyev/ru/netcalc/CalculatorApp.java
@@ -2,6 +2,8 @@ package asolovyev.ru.netcalc;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 
@@ -9,6 +11,7 @@ public class CalculatorApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath(getString(R.string.roboto_regular))
                 .setFontAttrId(R.attr.fontPath)
